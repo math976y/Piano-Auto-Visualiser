048659bdbb8d8a5cbaa9c2aa169a69da70da7a4e
math976y <157786723+math976y@users.noreply.github.com>
2026-03-02 11:23:14 +0100

    Implemented new samples

--- sketch.js
+++ sketch.js
@@ -7,18 +7,28 @@ function preload(){
   
   piano = {
     
-    "A4" : "Piano/A.mp3",
-    "A#4": "Piano/As.mp3",
-    "B4" : "Piano/B.mp3",
-    "C4" : "Piano/C.mp3",
-    "C#4": "Piano/Cs.mp3",
-    "D4" : "Piano/D.mp3",
-    "D#4": "Piano/Ds.mp3",
-    "E4" : "Piano/E.mp3",
-    "F4" : "Piano/F.mp3",
-    "F#4": "Piano/Fs.mp3",
-    "G4" : "Piano/G.mp3",
-    "G#4": "Piano/Gs.mp3"
+    "C2" : "Piano2/C2.wav",
+    "E2": "Piano2/E2.wav",
+    "G#2" : "Piano2/Gs2.wav",
+    "C3" : "Piano2/C3.wav",
+    "E3": "Piano2/E3.wav",
+    "G#3" : "Piano2/Gs3.wav",
+    "C4" : "Piano2/C4.wav",
+    "E4": "Piano2/E4.wav",
+    "G#4" : "Piano2/Gs4.wav",
+    "C5" : "Piano2/C5.wav",
+    "E5": "Piano2/E5.wav",
+    "G#5" : "Piano2/Gs5.wav",
+    "C6" : "Piano2/C6.wav",
+    "E6": "Piano2/E6.wav",
+    "G#6" : "Piano2/Gs6.wav",
+    "C7" : "Piano2/C7.wav",
+    "E7": "Piano2/E7.wav",
+    "G#7" : "Piano2/Gs7.wav",
+    "C8" : "Piano2/C8.wav",
+    "E8": "Piano2/E8.wav",
+    "G#8" : "Piano2/Gs8.wav",
+    "C9" : "Piano2/C9.wav",
     
   }
   
@@ -33,7 +43,7 @@ function preload(){
     
   }).toDestination();
 
-  const reverb = new Tone.Reverb(50).toDestination();
+  const reverb = new Tone.Reverb(100).toDestination();
   sampler.connect(reverb);
 
   music = loadJSON('hisaishi.json')
